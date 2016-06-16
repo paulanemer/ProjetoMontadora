95177089c60626b76d68cebf0253570514adb1aa
Paula Nemer <nemer.paula@gmail.com>
2016-06-16 16:18:02 -0300

    teste

---
+++ Principal/Projetos/ProjetoMontadora/src/main/java/ClasseDeTeste.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class ClasseDeTeste {
+
+	public static void main(String[] args) {
+		
+		System.out.println(StringUtils.capitalize("Teste Maven"));
+	}
+}
